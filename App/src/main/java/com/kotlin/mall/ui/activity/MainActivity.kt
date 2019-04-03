700e64b02d6bdf704d23904837c047e1b0b21628
张麻子 <beigua7357@gmail.com>
2019-04-03 23:29:10 +0800

    第一个小demo

--- App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
+++ App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.kotlin.mall.ui.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.kotlin.mall.R
 
 class MainActivity : AppCompatActivity() {
