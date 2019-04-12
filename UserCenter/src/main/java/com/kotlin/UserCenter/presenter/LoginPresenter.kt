297f8439bba6ab460eddad3a602fe70cb1d16632
zhangmazi <zq55210@sina.cn>
2019-04-12 16:44:18 +0800

    删除KotlinMall.iml

---
+++ UserCenter/src/main/java/com/kotlin/UserCenter/presenter/LoginPresenter.kt
@@ -0,0 +1,9 @@
+package com.kotlin.UserCenter.presenter
+
+/**
+ * @author : ${Zhang}
+ * @Date   : 2019/4/12 12:17:11
+ * @Des    :
+ */
+class LoginPresenter {
+}
---
+++ UserCenter/src/main/java/com/kotlin/UserCenter/presenter/view/LoginView.kt
@@ -0,0 +1,9 @@
+package com.kotlin.UserCenter.presenter.view
+
+/**
+ * @author : ${Zhang}
+ * @Date   : 2019/4/12 12:17:29
+ * @Des    :
+ */
+class LoginView {
+}
