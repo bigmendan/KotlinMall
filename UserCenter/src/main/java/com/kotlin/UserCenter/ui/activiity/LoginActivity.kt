70b98c322d7f0a3d6932cc2832385d926e4f3de9
zhangmazi <zq55210@sina.cn>
2019-04-12 16:55:20 +0800

    删除一些冗余文件，重新上传

--- UserCenter/src/main/java/com/kotlin/UserCenter/ui/activiity/LoginActivity.kt
+++ UserCenter/src/main/java/com/kotlin/UserCenter/ui/activiity/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.kotlin.UserCenter.ui.activiity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kotlin.UserCenter.R
 
 class LoginActivity : AppCompatActivity() {
 
