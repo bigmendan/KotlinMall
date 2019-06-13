aba0f1a7496b0798ffd7c079b384ce715586ab66
zhangmazi <zq55210@sina.cn>
2019-06-13 14:43:03 +0800

    添加一个直播 Module

---
+++ AVCenter/src/main/java/com/kotlin/avcenter/common/AvApplication.kt
@@ -0,0 +1,15 @@
+package com.kotlin.avcenter.common
+
+import android.app.Application
+
+/**
+ * @author : ${Zhang}
+ * @Date   : 2019/6/13 10:41:31
+ * @Des    :
+ */
+class AvApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ AVCenter/src/main/java/com/kotlin/avcenter/ui/activity/ZBActivity.kt
@@ -0,0 +1,13 @@
+package com.kotlin.avcenter.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.avcenter.R
+
+class ZBActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zb)
+    }
+}
