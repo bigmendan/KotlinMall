17a87b063b71732df1122b8346c966317cd53e17
zhangmazi <zq55210@sina.cn>
2019-06-13 17:38:01 +0800

    saaaaaaaaaaaaaa

--- AVCenter/src/main/java/com/kotlin/avcenter/common/AvApplication.kt
+++ AVCenter/src/main/java/com/kotlin/avcenter/common/AvApplication.kt
@@ -1,6 +1,7 @@
 package com.kotlin.avcenter.common
 
 import android.app.Application
+import com.tencent.rtmp.TXLiveBase
 
 /**
  * @author : ${Zhang}
@@ -10,6 +11,8 @@ import android.app.Application
 class AvApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
+        val licenceURL = "http://license.vod2.myqcloud.com/license/v1/df82dc8fb0631074dfb8daef413f1b40/TXLiveSDK.licence" // 获取到的 licence url
+        val licenceKey = "db1fda04ab75ac57fc0901bb470d2043" // 获取到的 licence key
+        TXLiveBase.getInstance().setLicence(this, licenceURL, licenceKey)
     }
 }
