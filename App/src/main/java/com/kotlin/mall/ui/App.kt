ec8fd2df76c3fad144c0a7937362b82c845fc0d7
张麻子 <beigua7357@gmail.com>
2019-06-19 17:09:39 +0800

    修改，，，

--- App/src/main/java/com/kotlin/mall/ui/App.kt
+++ App/src/main/java/com/kotlin/mall/ui/App.kt
@@ -2,7 +2,6 @@ package com.kotlin.mall.ui
 
 import android.app.Application
 import com.kotlin.base.common.BaseApplication
-import com.tencent.rtmp.TXLiveBase
 
 class App : BaseApplication() {
 
--- WanAndroid/src/main/java/com/kotlin/WanAndroid/common/App.kt
+++ WanAndroid/src/main/java/com/kotlin/WanAndroid/common/App.kt
@@ -1,7 +1,6 @@
 package com.kotlin.WanAndroid.common
 
 import com.kotlin.base.common.BaseApplication
-import com.tencent.rtmp.TXLiveBase
 
 class App : BaseApplication() {
 
@@ -9,8 +8,5 @@ class App : BaseApplication() {
     override fun onCreate() {
         super.onCreate()
 
-        val licenceURL = "http://license.vod2.myqcloud.com/license/v1/df82dc8fb0631074dfb8daef413f1b40/TXLiveSDK.licence" // 获取到的 licence url
-        val licenceKey = "db1fda04ab75ac57fc0901bb470d2043" // 获取到的 licence key
-        TXLiveBase.getInstance().setLicence(this, licenceURL, licenceKey)
     }
 }
